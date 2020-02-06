5de718f46a0fe0bd594f250a294e32ee10987aef
JinWen <onlystp417@gmail.com>
2020-02-06 13:14:57 +0800

    make fake router for develop

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <router-link to="/">home</router-link>|
+      <router-link to="/misisonHome">m-home</router-link>
+      <router-link to="/missionInstruction-one">m-i-one</router-link>
+      <router-link to="/missionInstruction-two">m-i-two</router-link>
+      <router-link to="/missionInstruction-three">m-i-three</router-link>
+      <router-link to="/missionInstruction-four">m-i-four</router-link>
+      <router-link to="/missionLevel">m-level</router-link>
+      <router-link to="/missionStart">m-start</router-link>
+      <router-link to="/rolcall">rolcall</router-link>
+      <router-link to="/course">course</router-link>
+      <router-link to="/quiz">quiz</router-link>
+      <router-link to="/privateMessage">private-m</router-link>
+      <router-link to="/publicMessage">public-m</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import home from '../views/home.vue'
+import missionHome from '../views/missionHome.vue'
+import missionInstructionOne from '../views/missionInstruction-one.vue'
+import missionInstructionTwo from '../views/missionInstruction-two.vue'
+import missionInstructionThree from '../views/missionInstruction-three.vue'
+import missionInstructionFour from '../views/missionInstruction-four.vue'
+import missionLevel from '../views/missionLevel.vue'
+import missionStart from '../views/missionStart.vue'
+import rollcall from '../views/rollcall.vue'
+import grade from '../views/grade.vue'
+import course from '../views/course.vue'
+import quiz from '../views/quiz.vue'
+import privateMessage from '../views/privateMessage.vue'
+import publicMessage from '../views/publicMessage.vue'
+
 
 Vue.use(VueRouter)
 
@@ -11,10 +25,70 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/About.vue')
-  }
+    path: '/missionHome',
+    name: 'missionHomet',
+    component: () => import('../views/missionHome.vue')
+  },
+  {
+    path: '/missionInstruction-one',
+    name: 'missionInstruction-one',
+    component: () => import('../views/missionInstruction-one.vue')
+  },
+  {
+    path: '/missionInstruction-two',
+    name: 'missionInstruction-two',
+    component: () => import('../views/missionInstruction-two.vue')
+  },
+  {
+    path: '/missionInstruction-three',
+    name: 'missionInstruction-three',
+    component: () => import('../views/missionInstruction-three.vue')
+  },
+  {
+    path: '/missionInstruction-four',
+    name: 'missionInstruction-four',
+    component: () => import('../views/missionInstruction-four.vue')
+  },
+  {
+    path: '/missionLevel',
+    name: 'missionLevel',
+    component: () => import('../views/missionLevel.vue')
+  },
+  {
+    path: '/missionStart',
+    name: 'missionStart',
+    component: () => import('../views/missionStart.vue')
+  },
+  {
+    path: '/rollcall',
+    name: 'rollcall',
+    component: () => import('../views/rollcall.vue')
+  },
+  {
+    path: '/grade',
+    name: 'grade',
+    component: () => import('../views/grade.vue')
+  },
+  {
+    path: '/course',
+    name: 'course',
+    component: () => import('../views/course.vue')
+  },
+  {
+    path: '/quiz',
+    name: 'quiz',
+    component: () => import('../views/quiz.vue')
+  },
+  {
+    path: '/privateMessage',
+    name: 'privateMessage',
+    component: () => import('../views/privateMessage.vue')
+  },
+  {
+    path: '/publicMessage',
+    name: 'publicMessage',
+    component: () => import('../views/publicMessage.vue')
+  },
 ]
 
 const router = new VueRouter({
