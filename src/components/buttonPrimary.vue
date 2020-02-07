52864542ec0b58a31ff4b419d1eaef30823e18a1
JinWen <onlystp417@gmail.com>
2020-02-07 18:24:11 +0800

    feat: components/buttonPrimary

--- src/components/buttonPrimary.vue
+++ src/components/buttonPrimary.vue
@@ -1,13 +1,29 @@
 <template>
-  <div></div>
+  <button class="primary-button">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+.primary-button {
+  display: block;
+  background: $light-blue;
+  border-radius: 21px;
+  margin: auto;
+  padding: 0;
+  width: 228px;
+  line-height: 42px;
+  color: $white;
+  font-size: 16px;
+  &:active {
+    box-sizing: border-box;
+    background: $white;
+    color: $light-blue;
+    border: $light-blue 2px solid;
+  }
+}
 </style>
