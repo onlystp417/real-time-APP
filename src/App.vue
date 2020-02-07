af7c535cfdb7ff783c4fec12f612366fb6095950
JinWen <onlystp417@gmail.com>
2020-02-07 11:47:15 +0800

    fix: router mount views

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
     <bar></bar>
+    <!-- <missionHome></missionHome> -->
     <div id="fake-router">
-      <router-link to="/">home</router-link>|
-      <router-link to="/misisonHome">m-home</router-link>|
+      <router-link to="/home">home</router-link>|
+      <router-link to="/missionHome">m-home</router-link>|
       <router-link to="/missionInstruction-one">m-i-one</router-link>|
-      <router-link to="/missionInstruction-two">m-i-two</router-link>|
+      <!-- <router-link to="/missionInstruction-two">m-i-two</router-link>|
       <router-link to="/missionInstruction-three">m-i-three</router-link>|
       <router-link to="/missionInstruction-four">m-i-four</router-link>|
       <router-link to="/missionLevel">m-level</router-link>|
@@ -14,7 +15,7 @@
       <router-link to="/course">course</router-link>|
       <router-link to="/quiz">quiz</router-link>|
       <router-link to="/privateMessage">private-m</router-link>|
-      <router-link to="/publicMessage">public-m</router-link>|
+      <router-link to="/publicMessage">public-m</router-link>|-->
     </div>
     <router-view></router-view>
   </div>
@@ -22,8 +23,7 @@
 
 <script>
 import bar from "@/components/bar.vue";
-// import info from "@/views/info.vue";
-// import missionStart from "@/views/missionStart.vue";
+// import missionHome from "@/views/missionHome.vue";
 
 export default {
   data: function() {
@@ -34,9 +34,13 @@ export default {
   },
   components: {
     bar
+    // missionHome
   }
 };
 </script>
 
 <style lang="scss">
+#app {
+  padding-top: 80px;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home"></div>
+  <div class="home">123</div>
 </template>
 
 <script>
