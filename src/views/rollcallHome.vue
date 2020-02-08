df35916d53747f95f3bbd79e03735ef486f3a090
JinWen <onlystp417@gmail.com>
2020-02-08 18:39:26 +0800

    feat: views/rollcallHome

--- src/views/rollcallHome.vue
+++ src/views/rollcallHome.vue
@@ -1,10 +1,59 @@
 <template>
-  <div></div>
+  <div class="rollcall-home">
+    <h3>點名紀錄</h3>
+    <section class="list-card">
+      <h4>最近點名紀錄</h4>
+      <buttonSecondary>查看</buttonSecondary>
+    </section>
+    <section class="list-card">
+      <h4>準時</h4>
+      <span>1</span>
+    </section>
+    <section class="list-card">
+      <h4>遲到</h4>
+      <span>2</span>
+    </section>
+    <section class="list-card">
+      <h4>未到</h4>
+      <span>0</span>
+    </section>
+    <section class="list-card">
+      <h4>請假</h4>
+      <span>0</span>
+    </section>
+    <section class="list-card">
+      <h4>出席率</h4>
+      <span>50％</span>
+    </section>
+    <buttonPrimary>我要點名</buttonPrimary>
+  </div>
 </template>
 
 <script>
-export default {};
+import buttonPrimary from "@/components/buttonPrimary.vue";
+import buttonSecondary from "@/components/buttonSecondary.vue";
+export default {
+  components: {
+    buttonPrimary,
+    buttonSecondary
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.rollcall-home {
+  width: 379px;
+  margin: auto;
+  padding-top: 15px;
+  h3 {
+    margin-bottom: 9px;
+    @include fontstyle(700, 24px, 1.2, $font, #474747);
+  }
+  .primary-button {
+    width: 200px;
+    margin-top: 48px;
+    font-size: 20px;
+    line-height: 44px;
+  }
+}
 </style>
