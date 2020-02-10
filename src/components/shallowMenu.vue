c9d1be0de89a0b5863226da105c1faecd5965c7e
JinWen <onlystp417@gmail.com>
2020-02-10 17:57:15 +0800

    feat: components/shallowMenu

--- src/components/shallowMenu.vue
+++ src/components/shallowMenu.vue
@@ -3,19 +3,19 @@
     <a class="rollcall rollcall-current" href>
       <h2>點名</h2>
     </a>
-    <a class="quiz quiz-current" href>
+    <a class="quiz" href>
       <h2>問答</h2>
     </a>
-    <a class="grade grade-current" href>
+    <a class="grade" href>
       <h2>成績</h2>
     </a>
-    <a class="course-info course-current" href>
+    <a class="course-info" href>
       <h2>課程資訊</h2>
     </a>
-    <a class="private private-current" href>
+    <a class="private" href>
       <h2>私訊老師</h2>
     </a>
-    <a class="public public-current" href>
+    <a class="public" href>
       <h2>公開討論</h2>
     </a>
   </nav>
@@ -31,50 +31,26 @@ export default {};
     width: 16.66667%;
   }
   .rollcall {
-    background: url(../assets/images/rollcall.png) top center/53px no-repeat;
-    &:active {
-      background: url(../assets/images/rollcall-active.png) top center/53px
-        no-repeat;
-    }
+    background: url(../assets/images/s-rollcall.png) top center/53px no-repeat;
   }
   .quiz {
-    background: url(../assets/images/rollcall.png) top center/53px no-repeat;
-    &:active {
-      background: url(../assets/images/rollcall-active.png) top center/53px
-        no-repeat;
-    }
+    background: url(../assets/images/s-quiz.png) top center/53px no-repeat;
   }
   .grade {
-    background: url(../assets/images/info.png) top center/53px no-repeat;
-    &:active {
-      background: url(../assets/images/info-active.png) top center/53px
-        no-repeat;
-    }
+    background: url(../assets/images/s-grade.png) top center/53px no-repeat;
   }
   .course-info {
-    background: url(../assets/images/info.png) top center/53px no-repeat;
-    &:active {
-      background: url(../assets/images/info-active.png) top center/53px
-        no-repeat;
-    }
+    background: url(../assets/images/s-course.png) top center/53px no-repeat;
   }
   .private {
-    background: url(../assets/images/message.png) top center/53px no-repeat;
-    &:active {
-      background: url(../assets/images/message-active.png) top center/53px
-        no-repeat;
-    }
+    background: url(../assets/images/s-private.png) top center/53px no-repeat;
   }
   .public {
     background: url(../assets/images/message.png) top center/53px no-repeat;
-    &:active {
-      background: url(../assets/images/message-active.png) top center/53px
-        no-repeat;
-    }
   }
 
   .rollcall-current {
-    background: url(../assets/images/rollcall-active.png) top center/53px
+    background: url(../assets/images/s-rollcall-active.png) top center/53px
       no-repeat;
     color: $light-blue;
     h2 {
@@ -82,7 +58,7 @@ export default {};
     }
   }
   .quiz-current {
-    background: url(../assets/images/rollcall-active.png) top center/53px
+    background: url(../assets/images/s-quiz-active.png) top center/53px
       no-repeat;
     color: $light-blue;
     h2 {
@@ -90,21 +66,23 @@ export default {};
     }
   }
   .grade-current {
-    background: url(../assets/images/info-active.png) top center/53px no-repeat;
+    background: url(../assets/images/s-grade-active.png) top center/53px
+      no-repeat;
     color: $light-blue;
     h2 {
       color: $light-blue;
     }
   }
   .course-info-current {
-    background: url(../assets/images/info-active.png) top center/53px no-repeat;
+    background: url(../assets/images/s-course-active.png) top center/53px
+      no-repeat;
     color: $light-blue;
     h2 {
       color: $light-blue;
     }
   }
   .private-current {
-    background: url(../assets/images/message-active.png) top center/53px
+    background: url(../assets/images/s-private-active.png) center 7px/32px
       no-repeat;
     color: $light-blue;
     h2 {
