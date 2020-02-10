c8521b2ceec60cef730a4145b1df1d024d4fad3d
JinWen <onlystp417@gmail.com>
2020-02-10 21:17:16 +0800

    feat: components/buttonSecondary

--- src/components/buttonSecondary.vue
+++ src/components/buttonSecondary.vue
@@ -1,13 +1,33 @@
 <template>
-  <div></div>
+  <div class="button-wrap">
+    <button class="secondary-button">
+      <slot></slot>
+    </button>
+  </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
+.button-wrap {
+  display: inline-block;
+  width: 25%;
+  text-align: right;
+}
 
+.secondary-button {
+  background: $light-blue;
+  width: 74px;
+  height: 32px;
+  border-radius: 9px;
+  box-sizing: border-box;
+  @include fontstyle(400, 16px, 22px, $font, $white);
+  &:active {
+    background: $white;
+    color: $light-blue;
+    border: $light-blue 2px solid;
+  }
+}
 </style>
