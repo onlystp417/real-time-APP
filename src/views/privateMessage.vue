8a57d2ab9d9f5fa9dcdb823d6899e11e1491987b
JinWen <onlystp417@gmail.com>
2020-02-10 20:07:46 +0800

    feat: deepSubMenu system router

--- src/views/privateMessage.vue
+++ src/views/privateMessage.vue
@@ -3,11 +3,19 @@
 </template>
 
 <script>
+import shallowMenu from "@/components/shallowMenu.vue";
+import deepMainMenu from "@/components/deepMainMenu.vue";
+import deepSubMenuRollcallQuiz from "@/components/deepSubMenu_rollcall_quiz.vue";
+import deepSubMenuMessage from "@/components/deepSubMenu_message.vue";
+import deepSubMenuGradeCourse from "@/components/deepSubMenu_grade_course.vue";
 export default {
-  
-}
+  shallowMenu,
+  deepMainMenu,
+  deepSubMenuRollcallQuiz,
+  deepSubMenuGradeCourse,
+  deepSubMenuMessage
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/views/rollcall.vue
+++ src/views/rollcall.vue
@@ -14,6 +14,8 @@
 import shallowMenu from "@/components/shallowMenu.vue";
 import deepMainMenu from "@/components/deepMainMenu.vue";
 import deepSubMenuRollcallQuiz from "@/components/deepSubMenu_rollcall_quiz.vue";
+import deepSubMenuMessage from "@/components/deepSubMenu_message.vue";
+import deepSubMenuGradeCourse from "@/components/deepSubMenu_grade_course.vue";
 import rollcallHome from "@/views/rollcallHome.vue";
 import rollcallEnroll from "@/views/rollcallEnroll.vue";
 import rollcallOntime from "@/views/rollcallOntime.vue";
@@ -23,6 +25,8 @@ export default {
     shallowMenu,
     deepMainMenu,
     deepSubMenuRollcallQuiz,
+    deepSubMenuGradeCourse,
+    deepSubMenuMessage,
     rollcallHome,
     rollcallEnroll,
     rollcallOntime,
