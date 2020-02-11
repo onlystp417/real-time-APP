5f3b3039ca762ce1ec6d8f953c3aa0cc971af533
YUN-RU-TSENG <coco002197@gmail.com>
2020-02-11 23:11:35 +0800

    fix: remove data of missionMessage to missionInstruction

--- src/store/index.js
+++ src/store/index.js
@@ -102,38 +102,7 @@ export default new Vuex.Store({
           }
         ]
       ]
-    },
-    messionMessage: [
-      [
-        "1.感受到振動後拿起手機",
-        "2.使用點名功能簽到",
-        "3.觀看本堂課的課程資訊",
-        "4.傳送「課程順暢」文字訊息給老師"
-      ],
-      [
-        "1.感受到振動後拿起手機",
-        "2.回答第一題，選擇A選項",
-        "3.說出答題是否正確",
-        "4.找到討論區的「邊緣運算」文章",
-        "5.在「邊緣運算」文章中 新增留言「雲端運算」",
-        "6.觀看本帳號的最近一次的點名紀錄"
-      ],
-      [
-        "1.感受到振動後拿起手機",
-        "2.回答第二題，選擇B選項",
-        "3.說出答題是否正確",
-        "4.查看本課程的出席率",
-        "5.查看本課程的「12/3停課一周」公告"
-      ],
-      [
-        "1.感受到振動後拿起手機",
-        "2.使用私訊老師的功能",
-        "3.以「我懂了」的選項回饋老師",
-        "4.查看本課程的答題正確率",
-        "5.觀看本課程的修課人數"
-      ]
-    ],
-    isAddToLocalStorage: "false"
+    }
   },
   getters: {
     chineseMissionLevel: state =>
@@ -150,8 +119,9 @@ export default new Vuex.Store({
     }
   },
   mutations: {
-    setMissionLevel: (state, payload) =>
-      (state.user.missionLevel = payload.level)
+    setMissionLevel: (state, payload) => {
+      state.user.missionLevel = payload.level
+    }
   },
   actions: {},
   modules: {}
