54ab23489592780210a8f24f308b6491e46ad9eb
YUN-RU-TSENG <coco002197@gmail.com>
2020-02-11 16:14:55 +0800

    feat: missionCompelete component method and fake data

--- src/store/index.js
+++ src/store/index.js
@@ -7,33 +7,100 @@ export default new Vuex.Store({
   state: {
     compelete: [],
     user: {
-      id: null,
-      name: null,
+      id: 23,
+      gender: "male",
+      name: "黃庭偉",
       missionState: [false, false, false, false],
-      missionLevel: null,
+      missionLevel: 2,
       missionLevelState: [
-        {
-          finish: false,
-          startTime: null,
-          endTime: null
-        },
-        {
-          finish: false,
-          startTime: null,
-          endTime: null
-        },
-        {
-          finish: false,
-          startTime: null,
-          endTime: null
-        },
-        {
-          finish: false,
-          startTime: null,
-          endTime: null
-        },
-        {},
-        {}
+        [
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          }
+        ],
+        [
+          {
+            finish: true,
+            startTime: new Date(2020, 2, 1, 1, 10),
+            endTime: new Date(2020, 2, 1, 1, 13)
+          },
+          {
+            finish: true,
+            startTime: new Date(2020, 2, 1, 1, 18),
+            endTime: new Date(2020, 2, 1, 1, 33)
+          },
+          {
+            finish: false,
+            startTime: new Date(2020, 2, 1, 1, 36),
+            endTime: new Date(2020, 2, 1, 1, 37)
+          },
+          {
+            finish: false,
+            startTime: new Date(2020, 2, 1, 1, 44),
+            endTime: new Date(2020, 2, 1, 1, 54)
+          }
+        ],
+        [
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          }
+        ],
+        [
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          },
+          {
+            finish: false,
+            startTime: null,
+            endTime: null
+          }
+        ]
       ]
     },
     messionMessage: [
@@ -68,7 +135,26 @@ export default new Vuex.Store({
     ],
     isAddToLocalStorage: "false"
   },
-  mutations: {},
+  getters: {
+    chineseMissionLevel: state =>
+      state.user.missionLevel.toLocaleString("zh-u-nu-hanidec"),
+    setGenderAbbreviation: state => (state.user.gender === "male" ? "M" : "F"),
+    setUserSymbol: (state, getters) =>
+      getters.setGenderAbbreviation + state.user.id.toString(),
+    setMissionMessage: state =>
+      state.messionMessage.findIndex(state.user.missionLevel + 1),
+    setMissionTime: state => {
+      return state.user.missionLevelState
+        .find((value, index) => index === state.user.missionLevel - 1)
+        .map(
+          value => (value.endTime.getTime() - value.startTime.getTime()) / 1000
+        );
+    }
+  },
+  mutations: {
+    setMissionLevel: (state, payload) =>
+      (state.user.missionLevel = payload.level)
+  },
   actions: {},
   modules: {}
 });
--- src/views/missionComplete.vue
+++ src/views/missionComplete.vue
@@ -1,40 +1,101 @@
 <template>
-  <div></div>
+  <div class="missionComplete-container">
+    <h2 class="missionComplete-title">任務{{ missionLevelChinese }}完成！</h2>
+    <section class="missionComplete-card">
+      <h3 class="card-title">
+        <span>編號：{{ userID }}</span>
+        <span>{{ userName }}</span>
+      </h3>
+      <div class="card-container">
+        <p
+          class="card-text"
+          v-for="(item, index) in messionMessage"
+          :key="index"
+        >
+          任務{{ missionLevel }}-{{ index }}：{{ item.minute }}分{{
+            item.second
+          }}秒
+        </p>
+      </div>
+    </section>
+    <div class="missionComplete-container-button">
+      <buttonPrimarySmall>開始</buttonPrimarySmall>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import buttonPrimarySmall from "@/components/buttonPrimarySmall.vue";
+export default {
+  components: {
+    buttonPrimarySmall
+  },
+  computed: {
+    missionLevelChinese() {
+      return this.$store.getters.chineseMissionLevel;
+    },
+    missionLevel() {
+      return this.$store.state.user.missionLevel;
+    },
+    userID() {
+      return this.$store.getters.setUserSymbol;
+    },
+    userName() {
+      return this.$store.state.user.name;
+    },
+    messionMessage() {
+      return this.$store.getters.setMissionTime.map(value => {
+        let time = {
+          second: null,
+          minute: null
+        };
+        time.second = (value % 60).toString().replace(/^\b(?=\d$)/, 0);
+        time.minute = ((value - time.second) / 60)
+          .toString()
+          .replace(/^\b(?=\d$)/, 0);
+        return time;
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.missionCompeleted {
-  border-radius: 13px;
-  width: 360px;
-  margin: 0 auto;
-  padding-top: 111px;
-  text-align: center;
-  .card {
-    background-color: #fff;
-    padding: 26px 37px 34px 37px;
+.missionComplete {
+  &-container {
+    width: 360px;
+    padding-top: 111px;
+    margin: 0 auto;
   }
-  h3 {
-    @include fontstyle(bold, 20px, 1.5, $font, #757575);
-    span {
-      padding-left: 12px;
+  &-title {
+    margin-bottom: 18px;
+    text-align: center;
+    @include fontstyle(900, 26px, 1.15, $font, #757575);
+  }
+  &-card {
+    background-color: $white;
+    border-radius: 13px;
+    padding: 26px 37px 34px 37px;
+    .card {
+      &-container {
+        text-align: center;
+      }
+      &-title {
+        text-align: center;
+        @include fontstyle(900, 20px, 1.15, $font, #757575);
+        margin-bottom: 23px;
+        span:first-child {
+          margin-right: 12px;
+        }
+      }
+      &-text {
+        @include fontstyle(700, 16px, 2, $font, #757575);
+      }
     }
   }
-  p {
-    @include fontstyle(bold, 16px, 2, $font, #757575);
-    margin: 0;
-  }
-  h2 {
-    @include fontstyle(regular, 26px, 1.5, $font, #757575);
-    margin-bottom: 18px;
+  &-container-button {
+    padding-top: 42px;
+    margin: 0 auto;
   }
 }
 </style>
-
-<script>
-export default {
-};
-</script>
