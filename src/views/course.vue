3e1c624ebb5ea2b2123ce9f4574200d49aa55fea
JinWen <onlystp417@gmail.com>
2020-02-12 11:15:37 +0800

    feat: views/courseHome

--- src/views/course.vue
+++ src/views/course.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="course">
-    <deepSubMenuRollcallQuiz></deepSubMenuRollcallQuiz>
-    <deepSubMenuGradeCourse></deepSubMenuGradeCourse>
-    <deepSubMenuMessage></deepSubMenuMessage>
-    <deepMainMenu></deepMainMenu>
+    <!-- <deepSubMenuRollcallQuiz></deepSubMenuRollcallQuiz> -->
+    <!-- <deepSubMenuGradeCourse></deepSubMenuGradeCourse> -->
+    <!-- <deepSubMenuMessage></deepSubMenuMessage> -->
+    <!-- <deepMainMenu></deepMainMenu> -->
     <shallowMenu></shallowMenu>
+    <courseHome></courseHome>
   </div>
 </template>
 
@@ -14,13 +15,16 @@ import deepMainMenu from "@/components/deepMainMenu.vue";
 import deepSubMenuRollcallQuiz from "@/components/deepSubMenu_rollcall_quiz.vue";
 import deepSubMenuMessage from "@/components/deepSubMenu_message.vue";
 import deepSubMenuGradeCourse from "@/components/deepSubMenu_grade_course.vue";
+import courseHome from "@/views/courseHome.vue";
+
 export default {
   components: {
     shallowMenu,
     deepMainMenu,
     deepSubMenuRollcallQuiz,
     deepSubMenuGradeCourse,
-    deepSubMenuMessage
+    deepSubMenuMessage,
+    courseHome
   }
 };
 </script>
---
+++ src/views/courseHome.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="course-home">
+    <section class="information">
+      <componentTitle>課程資訊</componentTitle>
+      <div class="information-card normal-card">
+        <div>
+          <h5>授課教師</h5>
+          <p class="font-style">測試員-老師</p>
+        </div>
+        <div>
+          <h5>修課人數</h5>
+          <p class="font-style">54</p>
+        </div>
+        <div>
+          <h5>修課日期</h5>
+          <p class="font-style">108/9/10 ~ 109/1/10</p>
+        </div>
+      </div>
+    </section>
+    <section class="annousemenet">
+      <componentTitle>公告</componentTitle>
+      <div class="annousemenet-card normal-card">
+        <h5 class="font-style">12/3停課一周</h5>
+        <div class="button-wrapper">
+          <buttonSecondary></buttonSecondary>
+        </div>
+        <p>
+          因老師12/4要出國參加國際會議，
+          <br />11/13，11/20，11/27三週上課時改......
+        </p>
+      </div>
+      <div class="annousemenet-card normal-card">
+        <h5 class="font-style">下周作業</h5>
+        <div class="button-wrapper">
+          <buttonSecondary></buttonSecondary>
+        </div>
+        <p>下周作業為顧客旅程地圖一張</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import componentTitle from "@/components/componentTitle.vue";
+import buttonSecondary from "@/components/buttonSecondary.vue";
+export default {
+  components: {
+    componentTitle,
+    buttonSecondary
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.course-home {
+  width: 379px;
+  margin: auto;
+  padding-top: 28px;
+  .information {
+    &-card {
+      padding: 10px 16px 6.5px 16px;
+      margin-bottom: 18px;
+      > div + div {
+        border-top: 1px solid #70707022;
+      }
+    }
+    h5 {
+      display: inline-block;
+      margin-left: -4px;
+      @include fontstyle(100, 12px, 1, $font, #b7b7b7);
+      -webkit-transform: scale(0.83);
+    }
+    p {
+      margin-bottom: 8.5px;
+      margin-top: -4px;
+    }
+  }
+  .annousemenet-card {
+    position: relative;
+    padding: 8px 16px 6px 16px;
+    margin-bottom: 15px;
+    h5 {
+      margin-bottom: 9px;
+    }
+    p {
+      height: 45px;
+      @include fontstyle(300, 12px, 16px, $font, #474747);
+    }
+    .button-wrapper {
+      position: absolute;
+      top: 8px;
+      right: 12px;
+    }
+  }
+}
+
+.font-style {
+  @include fontstyle(700, 14px, 22px, $font, #474747);
+}
+</style>
