bc81c084eca6d731b6110473b36e6a5281cda7f2
JinWen <onlystp417@gmail.com>
2020-02-12 17:39:49 +0800

    fix: menu position fixed

--- src/components/deepSubMenu_rollcall_quiz.vue
+++ src/components/deepSubMenu_rollcall_quiz.vue
@@ -16,34 +16,4 @@ export default {};
 </script>
 
 <style lang="scss">
-.deep-sub-menu-wrapper {
-  display: relative;
-  padding-top: 39px;
-}
-
-.deep-sub-menu {
-  position: absolute;
-  top: 80px;
-  right: 0;
-  left: 0;
-  background: $white;
-  a {
-    display: inline-block;
-    width: 50%;
-    text-align: center;
-  }
-  span {
-    display: inline-block;
-    height: 39px;
-    box-sizing: border-box;
-    padding: 0 5px;
-    @include fontstyle(400, 20px, 37px, $font, #b7b7b7);
-  }
-  .current {
-    span {
-      color: $light-blue;
-      border-bottom: 2px solid $light-blue;
-    }
-  }
-}
 </style>
