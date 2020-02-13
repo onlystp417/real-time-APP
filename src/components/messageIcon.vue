1123668ab6d87932d1379c45e1a37b127d121d5f
JinWen <onlystp417@gmail.com>
2020-02-13 11:22:23 +0800

    feat: components/messageIcon

--- src/components/messageIcon.vue
+++ src/components/messageIcon.vue
@@ -1,13 +1,79 @@
 <template>
-  <div></div>
+  <div class="message-icon-wrapper">
+    <filterPopover></filterPopover>
+    <section class="message-icon">
+      <h4>與老師互動</h4>
+      <p>你可以透過文字或貼圖與老師進行互動</p>
+      <div class="message-item">
+        <button>
+          <img src="../assets/images/understand.png" alt="smiling face" />
+          <span>我懂了</span>
+        </button>
+        <button>
+          <img src="../assets/images/frustrat.png" alt="frustrated face" />
+          <span>聽不懂</span>
+        </button>
+        <button>
+          <img src="../assets/images/confuse.png" alt="confused face" />
+          <span>太快了</span>
+        </button>
+        <button>
+          <img src="../assets/images/typing.png" alt="message" />
+          <span>傳訊息</span>
+        </button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import filterPopover from "@/components/filterPopover.vue";
 export default {
-  
-}
+  components: {
+    filterPopover
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.message-icon {
+  position: absolute;
+  top: 57vh;
+  right: 16.5px;
+  left: 16.5px;
+  background: $white;
+  padding: 16px 24.5px 15px 24.5px;
+  border-radius: 5px;
+  z-index: 99999;
+  h4 {
+    margin-bottom: 13px;
+    @include fontstyle(500, 22px, 1.2, $font, #474747);
+  }
+  p {
+    margin-bottom: 19px;
+    @include fontstyle(500, 18px, 1.2, $font, #474747);
+  }
+}
 
+.message-item {
+  display: flex;
+  flex-wrap: wrap;
+  button {
+    flex: 0 0 50%;
+    background: $white;
+    padding: 10px 0;
+    margin-bottom: 5px;
+    text-align: left;
+    img {
+      width: 29px;
+      height: 29px;
+      margin-right: 12px;
+      vertical-align: middle;
+    }
+    span {
+      vertical-align: middle;
+      @include fontstyle(500, 18px, 1.2, $font, #474747);
+    }
+  }
+}
 </style>
