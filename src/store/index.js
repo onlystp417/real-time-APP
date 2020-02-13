ec90cafa087d04311be01af65d730550e89592bd
YUN-RU-TSENG <coco002197@gmail.com>
2020-02-13 17:19:03 +0800

    feat: splite file from stores

--- src/store/index.js
+++ src/store/index.js
@@ -1,132 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import state from "./state";
+import getters from "./getter";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    compelete: [],
-    user: {
-      id: 23,
-      gender: "male",
-      name: "黃庭偉",
-      missionState: [false, false, false, false],
-      missionLevel: 2,
-      missionLevelState: [
-        [
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          }
-        ],
-        [
-          {
-            finish: true,
-            startTime: new Date(2020, 2, 1, 1, 10),
-            endTime: new Date(2020, 2, 1, 1, 13)
-          },
-          {
-            finish: true,
-            startTime: new Date(2020, 2, 1, 1, 18),
-            endTime: new Date(2020, 2, 1, 1, 33)
-          },
-          {
-            finish: false,
-            startTime: new Date(2020, 2, 1, 1, 36),
-            endTime: new Date(2020, 2, 1, 1, 37)
-          },
-          {
-            finish: false,
-            startTime: new Date(2020, 2, 1, 1, 44),
-            endTime: new Date(2020, 2, 1, 1, 54)
-          }
-        ],
-        [
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          }
-        ],
-        [
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          },
-          {
-            finish: false,
-            startTime: null,
-            endTime: null
-          }
-        ]
-      ]
-    }
-  },
-  getters: {
-    chineseMissionLevel: state => {
-      return state.user.missionLevel.toLocaleString("zh-u-nu-hanidec");
-    },
-    setGenderAbbreviation: state => {
-      return state.user.gender === "male" ? "M" : "F";
-    },
-    userSymbol: (state, getters) => {
-      return getters.setGenderAbbreviation + state.user.id.toString();
-    },
-    setMissionTime: state => {
-      return state.user.missionLevelState
-        .find((value, index) => index === state.user.missionLevel - 1)
-        .map(
-          value => (value.endTime.getTime() - value.startTime.getTime()) / 1000
-        );
-    }
-  },
+  state,
+  getters,
   mutations: {
     setMissionLevel: (state, payload) => {
       state.user.missionLevel = payload.level;
     }
-  },
-  actions: {},
-  modules: {}
+  }
 });
